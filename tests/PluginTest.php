dce28e6c0239e6de2397a3e9ff40dac24fa1da46
Chris Chrisostomou <stuff@mou.me.uk>
2015-02-15 18:07:59 +0000

    docs; minimum-stability stable; bumped copyright year; dependencies updated

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -3,7 +3,7 @@
  * Phergie plugin for returning randomly generated numbers in response to dice rolling requests
  *
  * @link https://github.com/chrismou/phergie-irc-plugin-react-dice for the canonical source repository
- * @copyright Copyright (c) 2014 Chris Chrisostomou (http://mou.me)
+ * @copyright Copyright (c) 2015 Chris Chrisostomou (https://mou.me)
  * @license http://phergie.org/license New BSD License
  * @package Chrismou\Phergie\Plugin\Dice
  */
